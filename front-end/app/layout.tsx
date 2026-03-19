302beab716c6a6fa38b22093127dfa215035e879
undraex <undrahx@gmail.com>
2026-03-20 04:15:34 +0800

    distribution-header conflict fixed

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 import "./globals.css";
 import { GraphqlProvider } from "./providers/GraphqlProvider";
-
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist",
-});
 
 export const metadata: Metadata = {
   title: "Team 10 Asset Frontend",
@@ -20,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geist.variable} antialiased`}>
+      <body className="antialiased">
         <GraphqlProvider>{children}</GraphqlProvider>
       </body>
     </html>
